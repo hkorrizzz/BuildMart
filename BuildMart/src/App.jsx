3ccb48a6a650c22cae6e39a668eb98ecbc8ac833
kr <tutvse253@gmail.com>
2026-03-03 15:19:52 +0300

    feat(ui): add footer

---
+++ BuildMart/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Header } from './components/header/header'
+import { Footer } from './components/footer/footer'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Footer />
+    </>
+  )
+}
+
+export default App
